5ae86b17626d74c6cffefcf3f02ebdeb248a00f9
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-07-12 21:27:06 +1000

    Вспомогательный класс для базиса

--- Edge3D.cs
+++ Edge3D.cs
@@ -2,22 +2,15 @@ namespace VectorFEM3D;
 
 public class Edge3D
 {
-    public double X0 { get; set; }
-    public double Y0 { get; set; }
-    public double Z0 { get; set; }
-    public double X1 { get; set; }
-    public double Y1 { get; set; }
-    public double Z1 { get; set; }
+    public Point3D Point0 { get; set; }
+    public Point3D Point1 { get; set; }
     public double Length { get; set; }
 
     public Edge3D(Point3D point0, Point3D point1)
     {
-        X0 = point0.X;
-        Y0 = point0.Y;
-        Z0 = point0.Z;
-        X1 = point1.X;
-        Y1 = point1.Y;
-        Z1 = point1.Z;
-        Length = Math.Sqrt(Math.Pow(X1 - X0, 2) + Math.Pow(Y1 - Y0, 2) + Math.Pow(Z1 - Z0, 2));
+        Point0 = point0;
+        Point1 = point1;
+        Length = Math.Sqrt(Math.Pow(Point1.X - Point0.X, 2) + Math.Pow(point1.Y - point0.Y, 2) +
+                           Math.Pow(point1.Z - point0.Z, 2));
     }
 }
---
+++ Vector3D.cs
@@ -0,0 +1,18 @@
+﻿namespace VectorFEM3D;
+
+public class Vector3D
+{
+    private readonly double _x;
+    private readonly double _y;
+    private readonly double _z;
+
+    public Vector3D(double x, double y, double z)
+    {
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+
+    public static double operator *(Vector3D vector1, Vector3D vector2)
+        => vector1._x * vector2._x + vector1._y * vector2._y + vector1._z * vector2._z;
+}

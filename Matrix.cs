8cf2d20ad3d4e90c41310a44e70f7a27fb6ec862
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-07-12 22:04:46 +1000

    Вспомогательные классы матрицы и вектора

---
+++ Matrix.cs
@@ -0,0 +1,63 @@
+﻿namespace VectorFEM3D;
+
+public class Matrix
+{
+    private double[,] _mat;
+    public int Size { get; }
+
+    public double this[int i, int j]
+    {
+        get => _mat[i, j];
+        set => _mat[i, j] = value;
+    }
+
+    public Matrix(int size)
+    {
+        _mat = new double[size, size];
+        Size = size;
+    }
+
+    public void Clear()
+        => Array.Clear(_mat, 0, _mat.Length);
+
+    public void Copy(Matrix destination)
+    {
+        for (int i = 0; i < destination.Size; i++)
+        {
+            for (int j = 0; j < destination.Size; j++)
+            {
+                destination[i, j] = _mat[i, j];
+            }
+        }
+    }
+
+    public static Matrix operator +(Matrix fstMatrix, Matrix sndMatrix)
+    {
+        Matrix resultMatrix = new(fstMatrix.Size);
+
+        for (int i = 0; i < resultMatrix.Size; i++)
+        {
+            for (int j = 0; j < resultMatrix.Size; j++)
+            {
+                resultMatrix[i, j] = fstMatrix[i, j] + sndMatrix[i, j];
+            }
+        }
+
+        return resultMatrix;
+    }
+   
+    public static Matrix operator *(double coef, Matrix Mat)
+    {
+        Matrix resultMatrix = new(Mat.Size);
+
+        for (int i = 0; i < resultMatrix.Size; i++)
+        {
+            for (int j = 0; j < resultMatrix.Size; j++)
+            {
+                resultMatrix[i, j] = coef * Mat[i, j];
+            }
+        }
+
+        return resultMatrix;
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,118 @@
+﻿namespace VectorFEM3D;
+
+public class Vector
+{
+   private double[] _vec;
+   public int Length { get; }
+
+   public Vector(int dim)
+   {
+      _vec = new double[dim];
+      Length = dim;
+   }
+
+   public double this[int index]
+   {
+      get => _vec[index];
+      set => _vec[index] = value;
+   }
+
+   public void Randomize()
+   {
+      for (int i = 0; i < Length; i++)
+         _vec[i] = new Random().Next(1, 10);
+   }
+
+   public void Fill(double value)
+   {
+      for (int i = 0; i < Length; i++)
+      {
+         _vec[i] = value;
+      }
+   }
+
+   public static void Copy(Vector source, Vector destination)
+   {
+      for (int i = 0; i < source.Length; i++)
+         destination[i] = source[i];
+   }
+
+   public void Norming()
+   {
+      double norm = Norm();
+
+      for (int i = 0; i < Length; i++)
+         _vec[i] /= norm;
+   }
+
+   public double Norm()
+   {
+      double result = 0;
+
+      for (int i = 0; i < Length; i++)
+         result += _vec[i] * _vec[i];
+
+      return Math.Sqrt(result);
+   }
+
+   public double NormFEM()
+   {
+      double result = 0;
+
+      for (int i = 1; i < Length - 1; i++)
+         result += _vec[i] * _vec[i];
+
+      return Math.Sqrt(result);
+   }
+
+   public static Vector operator *(Matrix matrix, Vector vector)
+   {
+      Vector result = new(vector._vec.Length);
+
+      for (int i = 0; i < vector.Length; i++)
+         for (int j = 0; j < vector.Length; j++)
+            result._vec[i] += matrix[i, j] * vector._vec[j];
+
+      return result;
+   }
+
+   public static Vector operator -(Vector fstVector, Vector sndVector)
+   {
+      Vector result = new(fstVector.Length);
+
+      for (int i = 0; i < fstVector.Length; i++)
+         result[i] = fstVector._vec[i] - sndVector._vec[i];
+
+      return result;
+   }
+
+   public static Vector operator +(Vector fstVector, Vector sndVector)
+   {
+      Vector result = new(fstVector.Length);
+
+      for (int i = 0; i < fstVector.Length; i++)
+         result[i] = fstVector._vec[i] + sndVector._vec[i];
+
+      return result;
+   }
+
+   public static Vector operator *(double coef, Vector vector)
+   {
+      Vector result = new(vector.Length);
+
+      for (int i = 0; i < vector.Length; i++)
+         result[i] = vector._vec[i] * coef;
+
+      return result;
+   }
+
+   public static double operator *(Vector fstVector, Vector sndVector)
+   {
+      double result = 0;
+
+      for (int i = 0; i < fstVector.Length; i++)
+         result += fstVector._vec[i] * sndVector._vec[i];
+
+      return result;
+   }
+}

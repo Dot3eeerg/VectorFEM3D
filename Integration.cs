ec897ee276970396888de1a3c7bab0145820b1db
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-07-12 21:32:36 +1000

    Численное интегрирование по области

---
+++ Integration.cs
@@ -0,0 +1,33 @@
+﻿namespace VectorFEM3D;
+
+public class Integration
+{
+    private readonly IEnumerable<QuadratureNode> _quadratures;
+
+    public Integration(IEnumerable<QuadratureNode> quadratures) => _quadratures = quadratures;
+
+    public double Gauss3D(Func<Point3D, double> psi)
+    {
+        double result = 0;
+        Point3D point = new(0, 0, 0);
+
+        foreach (var qi in _quadratures)
+        {
+            point.X = (qi.Node + 1) / 2.0;
+
+            foreach (var qj in _quadratures)
+            {
+                point.Y = (qj.Node + 1) / 2.0;
+
+                foreach (var qk in _quadratures)
+                {
+                    point.Z = (qk.Node + 1) / 2.0;
+
+                    result += psi(point) * qi.Weight * qj.Weight * qk.Weight;
+                }
+            }
+        }
+
+        return result / 8.0;
+    }
+}
---
+++ QuadratureNode.cs
@@ -0,0 +1,42 @@
+﻿namespace VectorFEM3D;
+
+public class QuadratureNode
+{
+    public double Node { get; }
+    public double Weight { get; }
+
+    public QuadratureNode(double node, double weight)
+    {
+        Node = node;
+        Weight = weight;
+    }
+}
+
+public static class Quadratures
+{
+    public static IEnumerable<QuadratureNode> SegmentGaussOrder9()
+    {
+        const int n = 5;
+        
+        double[] points =
+        {
+            0.0,
+            1.0 / 3.0 * Math.Sqrt(5 - 2 * Math.Sqrt(10.0 / 7.0)),
+            -1.0 / 3.0 * Math.Sqrt(5 - 2 * Math.Sqrt(10.0 / 7.0)),
+            1.0 / 3.0 * Math.Sqrt(5 + 2 * Math.Sqrt(10.0 / 7.0)),
+            -1.0 / 3.0 * Math.Sqrt(5 + 2 * Math.Sqrt(10.0 / 7.0))
+        };
+
+        double[] weights =
+        {
+            128.0 / 225.0,
+            (322.0 + 13.0 * Math.Sqrt(70.0)) / 900.0,
+            (322.0 + 13.0 * Math.Sqrt(70.0)) / 900.0,
+            (322.0 - 13.0 * Math.Sqrt(70.0)) / 900.0,
+            (322.0 - 13.0 * Math.Sqrt(70.0)) / 900.0
+        };
+
+        for (int i = 0; i < n; i++)
+            yield return new(points[i], weights[i]);
+    }
+}
